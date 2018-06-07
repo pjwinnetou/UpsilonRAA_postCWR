e6c7fcbcc7c18bbf673e9c5123dacd8817628862
JaeBeom Park <pjwinnetou@gmail.com>
2018-06-07 16:36:10 +0900

    up

---
+++ Systematic/runMerge_postCWR.sh
@@ -0,0 +1,7 @@
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(1,"Hi")'
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(2,"Hi")'
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(3,"Hi")'
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(1,"Lo")'
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(2,"Lo")'
+root -l -b -q 'mergeSystematicUnc_asym_postCWR_constrain.C+(3,"Lo")'
+rm *.so *.d *.pcm
